b921f2fa4093d38f1f64c99f287e84d1c7b373ec
BCHIIEONG <benson6397@gmail.com>
2026-02-10 14:23:26 +0800

    chore(repo): bootstrap first version

---
+++ lab-flow-report-system/src/main/java/cn/edu/jnu/labflowreport/LabFlowReportSystemApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.jnu.labflowreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabFlowReportSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabFlowReportSystemApplication.class, args);
+	}
+
+}
---
+++ lab-flow-report-system/src/test/java/cn/edu/jnu/labflowreport/LabFlowReportSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.jnu.labflowreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabFlowReportSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
